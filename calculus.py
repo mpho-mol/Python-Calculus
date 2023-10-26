57f9cbb3ac1583fec3e27dbfacc36d4847d21fee
mpho-mol <143937796+mpho-mol@users.noreply.github.com>
2023-10-26 13:22:22 +0200

    Add files via upload

--- calculus.py
+++ calculus.py
@@ -1,31 +1,58 @@
 import numpy as np
-x = np.array([0.5, 1.5, 3, 4.25, 5, 7, 9.75])
 
+X = np.array([0.5, 1.5, 3, 4.25, 5, 7, 9.75])
 y = np.array([0, 4.5, 7, 6.5, 9, 20.25, 15.75])
-def model(x, m):
-   return m * x
 
+def model(x, m):
+    return m * x
 
 def error_function(m):
-   return 3 * (m**2)
+    # Define the numerator of the error function
+    numerator = 0
+    n = len(X)
+    for i in range(n):
+        y_true = y[i]
+        x_val = X[i]
+        numerator += (y_true - model(x_val, m))**2
 
-def derivative(n):
-   return 6 * m
-   
+    return numerator / n
+
+
+def derivative(m):
+    numerator = 0
+    n = len(X)
+    for i in range(n):
+        y_true = y[i]
+        x_val = X[i]
+        # Calculate inner expression
+        bracketed_term = y_true - model(x_val, m)
+        numerator += -2 * x_val * bracketed_term
+    return numerator / n
+
+
 def update_step(m, learning_rate=0.01):
-  return m - (learning_rate * derivative(m))
-m=-6
-n_epochs=5
-   
-  
+    # Get the amount to change m by
+    derivative_term = derivative(m)
+    update_amount = learning_rate * derivative_term
+
+    # Change m
+    return m - update_amount
+
+m = -6
+n_epochs = 5 # This is just the number of update steps you take
+
+# Iterate for n epochs
 for epoch in range(n_epochs):
-  m = update_step(m) 
-  print(f'Epoch {epoch + 1}: m = {round (m,2)} and loss = {round (error_function (m), 2)}') 
+    # Update and print m
+    m = update_step(m)
+    print(f'Epoch {epoch + 1}: m = {round(m,2)} and loss = {round(error_function(m), 2)}')
 
-print("predictions in x value")
+# Print Final Results
+print("-----------------")
 print("FINAL RESULTS")
-print("predictions in y values")
+print("-----------------")
 
-print (f'm= {round (m, 2)} and loss = {round (error_function (m), 2)}')
+print(f'm = {round(m,2)} and loss = {round(error_function(m), 2)}')
 
-print(f"The regression line is: y = {round(m, 2)}x")
+
+print("-----------------")
